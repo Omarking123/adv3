69d12b14729e5d8b4579d7ebda40e17175c01064
Omar <Omar@DESKTOP-OUKQKJG>
2025-01-31 09:35:28 +0300

    Video2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace adv3
 {
+    public delegate int StringFuncDelegate(string S);
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region video 2
+            //StringFuncDelegate x = StringFunctions.GetCountOfUpperCaseChars;
+
+            //x = StringFunctions.GetCountOfLowerCaseChars;
+            //int count = x.Invoke("Hello Word");
+
+            //Console.WriteLine(count); 
+            #endregion
+
         }
     }
 }
---
+++ StringFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adv3
+{
+    internal class StringFunctions
+    {
+        public static int GetCountOfUpperCaseChars(string Word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(Word))
+            {
+                for(int i=0; i < Word.Length; i++)
+                {
+                    if (char.IsUpper(Word[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+        public static int GetCountOfLowerCaseChars(string Word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(Word))
+            {
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    if (char.IsLower(Word[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
